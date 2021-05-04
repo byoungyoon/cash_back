1147e1dddb456b354ca5ef1bbd87186c44371c40
byoungyoon <70301218+byoungyoon@users.noreply.github.com>
2021-05-04 22:54:54 +0900

    cash project [21.5.4]
    1. cashbook Detail을 위한 새로운 vo작성
    
    2. cashbookNo를 쿼리문에서 출력할때 id로 전환(material ui 형식을 위해)

---
+++ cash/src/main/java/com/example/cash/vo/CashbookDetail.java
@@ -0,0 +1,12 @@
+package com.example.cash.vo;
+
+import lombok.Data;
+
+@Data
+public class CashbookDetail {
+	private int id;
+	private String cashbookInfo;
+	private String cashbookTitle;
+	private int cashbookPrice;
+	private String cashbookContent;
+}
